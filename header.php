e8ec8f25df64c745d229b575cff1ffd48f724663
KojakZoda <kabdullazoda@gmail.com>
2017-04-22 18:10:50 +0200

    Php mailer, bootstrap, jquery, font awesome added

--- header.php
+++ header.php
@@ -3,5 +3,8 @@
   <head>
     <meta charset="utf-8">
     <title>Artists Processes</title>
+    <link href="inc/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="inc/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
