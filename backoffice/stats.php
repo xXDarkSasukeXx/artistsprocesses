f2aebde5f0eae6540d718f68e392cfe0d9c90a89
KojakZoda <kabdullazoda@gmail.com>
2017-05-20 16:48:34 +0200

    Changed icon on admin dashboard

--- backoffice/stats.php
+++ backoffice/stats.php
@@ -18,7 +18,7 @@
       <div class="row">
         <div class="col-md-3 col-sm-6 col-xs-12">
           <div class="info-box">
-            <span class="info-box-icon bg-aqua"><i class="fa fa-picture-o" aria-hidden="true"></i></span>
+            <span class="info-box-icon bg-aqua"><i class="fa fa-user-o" aria-hidden="true"></i></span>
 
             <div class="info-box-content">
               <span class="info-box-text">Artistes</span>
