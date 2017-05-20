97bc0d13fd8010aeefcad135c00c65e284afd08a
KojakZoda <kabdullazoda@gmail.com>
2017-05-20 16:12:20 +0200

    Changed admin lte

--- backoffice/index.php
+++ backoffice/index.php
@@ -1,9 +1,26 @@
-<?php session_start(); ?>
+<?php
+  session_start();
+?>
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Artists Processes</title>
+    <title>Artists Processes | Dashboard</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Bootstrap 3.3.6 -->
+    <link rel="stylesheet" href="../inc/bootstrap/css/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- jvectormap -->
+    <link rel="stylesheet" href="plugins/jvectormap/jquery-jvectormap-1.2.2.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+    <!-- AdminLTE Skins. Choose a skin from the css/skins
+         folder instead of downloading all of them to reduce the load. -->
+    <link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
     <link href="../inc/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../inc/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="../inc/slick/slick.css">
@@ -25,7 +42,7 @@
 
     <div class="container-fluid">
       <div class="row">
-        <div class="sidebar">
+        <div class="sidebar-ap">
           <div class="sidebarHeader">
             <div class="toggle_button">
               <i class="fa fa-bars" aria-hidden="true"></i>
@@ -67,10 +84,10 @@
               <li role="presentation"><a href="#contact" aria-controls="contact" role="tab" data-toggle="tab">Contact</a></li>
             </ul>
         </div>
-        <div class="mainBackoffice">
-          <?php require('tabs.php'); ?>
-        </div>
+      </div>
+      <div class="mainBackoffice">
+        <?php require('tabs.php'); ?>
       </div>
     </div>
 
-<?php require('../shared/footer.php'); ?>
+<?php require('footer_back.php'); ?>
